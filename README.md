cd0b68bde48cb68f68e7ebb9111c6598c64d919d
Ivanov Alexandr <san2012ches@gmail.com>
2022-09-20 00:08:59 +0300

    Fixes for release version

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # RTU-MIREA-Schedule-Generator by raOvOen
 Simple excel document parser and word document generator for RTU MIREA schedule.
-Use the configuration file to set your own settings.
+
+To use the program, the configuration file "config.yaml" must be located in the same directory as it. Don't forget to specify in it the name of your group, as well as set the name of Excel document schedule ("schedule.xlsx" by default) and Word document output ("output.docx" by default). The input Excel document must also be in the same directory as the executable file.
--- Schedule.py
+++ Schedule.py
@@ -2,9 +2,10 @@ import openpyxl
 import yaml
 import datetime
 import docx
-# from docx.enum.table import WD_TABLE_ALIGNMENT
 from docx.shared import Pt
 from docx.shared import Mm
+from docx.oxml.ns import nsdecls
+from docx.oxml import parse_xml
 
 class Pair:
     def __init__(self, name, type, teacher, place):
@@ -21,10 +22,30 @@ class Pair:
     def get_place(self):
         return self.place
 
+def set_autofit(doc):
+    for t_idx, table in enumerate(doc.tables):
+        doc.tables[t_idx].autofit = True
+        doc.tables[t_idx].allow_autofit = True
+        doc.tables[t_idx]._tblPr.xpath("./w:tblW")[0].attrib["{http://schemas.openxmlformats.org/wordprocessingml/2006/main}type"] = "auto"
+        for row_idx, r_val in enumerate(doc.tables[t_idx].rows):
+            for cell_idx, c_val in enumerate(doc.tables[t_idx].rows[row_idx].cells):
+                doc.tables[t_idx].rows[row_idx].cells[cell_idx]._tc.tcPr.tcW.type = 'auto'
+                doc.tables[t_idx].rows[row_idx].cells[cell_idx]._tc.tcPr.tcW.w = 0
+    return doc
+
 def remove_row(table, del_row):
     tbl = table._tbl
     tr = del_row._tr
     tbl.remove(tr)
+
+def trim_useless(pair_name_to_write, exclude_words):
+    for i in pair_name_to_write.split():
+        if i in exclude_words:
+            return pair_name_to_write.split(i)[0]
+        if i.split(',')[0].isdigit():
+            return pair_name_to_write.split(i)[0]
+    return pair_name_to_write
+
 
 def parse_pair_name(pair_obj, row, week_numb, except_week_words, week_words):
     pair_name = str(pair_obj.get_name())
@@ -35,23 +56,31 @@ def parse_pair_name(pair_obj, row, week_numb, except_week_words, week_words):
     write_flag = True
     pair_name_list = pair_name.split()
     include_counter = 0
-    for i in range(0, len(pair_name_list)):
+    i=0
+    while i < len(pair_name_list):
         if pair_name_list[i] in except_week_words:
             except_weeks = []
-            i=i+1
+            i+=1
             while(pair_name_list[i] not in week_words):
                 for splitted_except_weeks in pair_name_list[i].split(','):
                     if splitted_except_weeks != '':
                         except_weeks.append(str(splitted_except_weeks))
-                i=i+1
+                i+=1
             if (str(week_numb) in except_weeks):
                 write_flag = False
-                break
+                i+=1
             else:
                 write_flag = True
                 pair_name_to_write = ""
                 for j in range(i+1, len(pair_name_list)):
                     pair_name_to_write += pair_name_list[j] + " "
+                try:
+                    type_to_write = (pair_obj.get_type().split("\n"))[include_counter-1]
+                    teacher_to_write = (pair_obj.get_teacher().split("\n"))[include_counter-1]
+                    place_to_write = (pair_obj.get_place().split("\n"))[include_counter-1]
+                except Exception as e:
+                    pass
+                pair_name_to_write = trim_useless(pair_name_to_write, except_week_words)
                 break
         elif ((pair_name_list[i].split(','))[0].isdigit()):
             include_counter += 1
@@ -61,17 +90,23 @@ def parse_pair_name(pair_obj, row, week_numb, except_week_words, week_words):
                     if splitted_include_weeks != '':
                         include_weeks.append(str(splitted_include_weeks))
                 i=i+1
-                pair_name_list[i].split(',')
             if (str(week_numb) in include_weeks):
                 write_flag = True
                 pair_name_to_write = ""
                 for j in range(i+1, len(pair_name_list)):
                     pair_name_to_write += pair_name_list[j] + " "
-                type_to_write = (pair_obj.get_type().split("\n"))[include_counter-1]
-                teacher_to_write = (pair_obj.get_teacher().split("\n"))[include_counter-1]
-                place_to_write = (pair_obj.get_place().split("\n"))[include_counter-1]
+                try:
+                    type_to_write = (pair_obj.get_type().split("\n"))[include_counter-1]
+                    teacher_to_write = (pair_obj.get_teacher().split("\n"))[include_counter-1]
+                    place_to_write = (pair_obj.get_place().split("\n"))[include_counter-1]
+                except Exception as e:
+                    pass
+                pair_name_to_write = trim_useless(pair_name_to_write, except_week_words)
+                break
             else:
                 write_flag = False
+        i+=1
+
     if(write_flag):
         row.cells[3].text = pair_name_to_write
         row.cells[4].text = type_to_write
@@ -79,7 +114,7 @@ def parse_pair_name(pair_obj, row, week_numb, except_week_words, week_words):
         row.cells[6].text = place_to_write
 
 if __name__ == "__main__":
-    print("Simple RTU MIREA Parser. Made by raOvOen (Thx to feodoriter and V8PDL)")
+    print("[OvO] Simple RTU MIREA Parser. Made by raOvOen (Thx to feodoriter and V8PDL)")
     try:
         print("[*] Opening 'config.yaml' file")
         with open("config.yaml", "r", encoding="UTF-8") as ymlfile:
@@ -106,6 +141,8 @@ if __name__ == "__main__":
     custom_width_flag = bool(cfg["additional"]["custom_width_flag"])
     custom_width_in_mm = cfg["additional"]["custom_width"].split()
     weekdays = cfg["additional"]["weekdays"].split()
+    coloring_weekdays = bool(cfg["main"]["coloring_weekdays"])
+    weekdays_colors = cfg["additional"]["weekdays_colors"].split()
     except_week_words_list = except_week_words.split()
     week_words_list = week_words.split()
     source_book = openpyxl.load_workbook(source_path)
@@ -153,18 +190,26 @@ if __name__ == "__main__":
     section.top_margin = Mm(top_margin_in_mm)
     section.bottom_margin = Mm(bottom_margin_in_mm)
     table = []
-    for i in range(0,work_week_amount): #Неделя
+    for i in range(0,work_week_amount):
         print(f"[*] Parsing {i+1} week...")
         par = doc.add_paragraph()
         par_run = par.add_run(f"Неделя №{i+1}\n")
         par_run.font.size = Pt(16)
         par_fmt = par.paragraph_format
         par_fmt.alignment = 1
-        table.append(doc.add_table(rows=work_day_amount*len(pair_time), cols=7))
+        table.append(doc.add_table(rows=work_day_amount*len(pair_time) + 1, cols=7))
         table[i].style = table_style
-        for j in range(0,work_day_amount): #День
-            for q in range(0, len(pair_time)): #Пара
-                temp_row = table[i].rows[j*len(pair_time)+q]
+        header_row = table[i].rows[0]
+        header_row.cells[0].text = "Дата занятия"
+        header_row.cells[1].text = "Номер пары"
+        header_row.cells[2].text = "Время проведения"
+        header_row.cells[3].text = "Название дисциплины"
+        header_row.cells[4].text = "Тип пары"
+        header_row.cells[5].text = "Преподаватель"
+        header_row.cells[6].text = "Место проведения"
+        for j in range(0,work_day_amount):
+            for q in range(0, len(pair_time)):
+                temp_row = table[i].rows[j*len(pair_time)+q + 1]
                 temp_row.cells[0].text = date.strftime('%d.%m') + "\n" + weekdays[date.weekday()]
                 temp_row.cells[1].text = str(q+1)
                 temp_row.cells[2].text = pair_time[q]
@@ -181,6 +226,8 @@ if __name__ == "__main__":
         for row in table[i].rows:
             if(row.cells[3].text == "None" or row.cells[3].text == ""):
                 remove_row(table[i], row)
+        if((len(table[i].rows) == 1)):
+            remove_row(table[i], table[i].rows[0])
     if (custom_width_flag):
         print("[*] Setting custom width...")
         for i in range(0,len(table)):
@@ -189,13 +236,21 @@ if __name__ == "__main__":
                 for cell in row.cells:
                     cell.width = Mm(int(custom_width_in_mm[q]))
                     q += 1
-    # if (not custom_width_flag):
-    #     print("[*] Autofitting column width...")
-    #     for i in range(0,len(table)):
-    #         table[i].alignment = WD_TABLE_ALIGNMENT.CENTER
-    #         table[i].allow_autofit = True
+    if (not custom_width_flag):
+        print("[*] Autofitting column width...")
+        set_autofit(doc)
+    if (coloring_weekdays):
+        print("[*] Coloring the rows...")
+        for i in range(0, len(table)):
+            for row in table[i].rows:
+                for j in range(0, len(weekdays)):
+                    if (weekdays[j] in row.cells[0].text):
+                        for q in range(0, 7):
+                            temp_shading_elm = parse_xml(r'<w:shd {} w:fill="FFFFFF"/>'.replace('FFFFFF',weekdays_colors[j]).format(nsdecls('w')))
+                            row.cells[q]._tc.get_or_add_tcPr().append(temp_shading_elm)
     print("[+] Parsing ended successfully!")
     doc.save(output_name)
+    input("[OvO] Press Enter to exit...")
 
 
 
